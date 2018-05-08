8875fd4eaf8a904a090bd87467da41526d37c526
Kevin Minehart <kmineh0151@gmail.com>
2018-05-08 03:08:54 -0500

    Adds proper Redis DBAL (#1)
    * ory-am -> ory
    
    * update function signatures
    
    * rebuild the redis manager
    
    * Add extra tests, fix update and delete
    
    * add extra test
    
    * Huge improvements to FindRequestCandidates performance

--- manager/redis/manager_redis.go
+++ manager/redis/manager_redis.go
@@ -1,12 +1,31 @@
+// The (new) redis manager is intended to look like the officially supported memory manager.
+// We take the tradeoff for slower inserts and deletions to make it easier to find applicable policies.
 package redis
 
 import (
 	"encoding/json"
+	"strings"
 
-	"github.com/pkg/errors"
 	"github.com/go-redis/redis"
-	. "github.com/ory-am/ladon"
+	. "github.com/ory/ladon"
+	"github.com/pkg/errors"
 )
+
+var (
+	ErrPolicyExists  = errors.New("Policy exists")
+	ErrBadConversion = errors.New("Could not convert policy from redis")
+)
+
+const (
+	prefixPolicy   = "policy"
+	prefixResource = "resource"
+	prefixSubject  = "subject"
+)
+
+// Just returns strings.Join(vals, "_") for creating redis keys
+func prefixKey(vals ...string) string {
+	return strings.Join(vals, "_")
+}
 
 // RedisManager is a redis implementation of Manager to store policies persistently.
 type RedisManager struct {
@@ -16,95 +35,260 @@ type RedisManager struct {
 
 // NewRedisManager initializes a new RedisManager with no policies
 func NewRedisManager(db *redis.Client, keyPrefix string) *RedisManager {
+	if keyPrefix == "" {
+		keyPrefix = "ladon"
+	}
+
 	return &RedisManager{
 		db:        db,
 		keyPrefix: keyPrefix,
 	}
 }
 
-const redisPolicies = "ladon:policies"
-
-var (
-	redisPolicyExists = errors.New("Policy exists")
-	redisNotFound     = errors.New("Not found")
-)
-
-func (m *RedisManager) redisPoliciesKey() string {
-	return m.keyPrefix + redisPolicies
-}
-
-// Create a new policy to RedisManager
+// Create a new policy in Redis. It will create a single key for the policy itself,
+// and for each subject and resource the policy will also exist in a hashmap.
 func (m *RedisManager) Create(policy Policy) error {
-	payload, err := json.Marshal(policy)
+	// Make sure that the key doesn't already exist
+	key := prefixKey(m.keyPrefix, prefixPolicy, policy.GetID())
+	if err := m.db.Get(key).Err(); err == nil {
+		return ErrPolicyExists
+	}
+
+	p, err := json.Marshal(policy)
 	if err != nil {
-		return errors.Wrap(err, "policy marshal failed")
+		return err
 	}
 
-	wasKeySet, err := m.db.HSetNX(m.redisPoliciesKey(), policy.GetID(), string(payload)).Result()
-	if !wasKeySet {
-		return errors.WithStack(redisPolicyExists)
-	} else if err != nil {
-		return errors.Wrap(err, "policy creation failed")
+	// Set the policy key
+	cmd := m.db.Set(key, p, 0)
+
+	if err := cmd.Err(); err != nil {
+		return err
 	}
 
+	// Put this policy in the hashmap for each resource
+	for _, v := range policy.GetResources() {
+		hmkey := prefixKey(m.keyPrefix, prefixResource, v)
+		field := policy.GetID()
+		if err := m.db.HMSet(hmkey, map[string]interface{}{
+			field: p,
+		}).Err(); err != nil {
+			return err
+		}
+	}
+
+	// Put this policy in the hashmap for each subject
+	for _, v := range policy.GetSubjects() {
+		hmkey := prefixKey(m.keyPrefix, prefixSubject, v)
+		field := policy.GetID()
+		if err := m.db.HMSet(hmkey, map[string]interface{}{
+			field: p,
+		}).Err(); err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
-// GetAll retrieves a all policy.
-func (m *RedisManager) GetAll() (Policies, error) {
-	return nil, errors.New("not implemented")
+// GetAll retrieves all policies. (Equivelant of db.keys + db.Mget)
+func (m *RedisManager) GetAll(limit int64, offset int64) (Policies, error) {
+	key := prefixKey(m.keyPrefix, prefixPolicy, "*")
+	keyscmd := m.db.Keys(key)
+	if err := keyscmd.Err(); err != nil {
+		return nil, err
+	}
+
+	keys, err := keyscmd.Result()
+	if err != nil {
+		return nil, err
+	}
+
+	mgetcmd := m.db.MGet(keys...)
+	if err := mgetcmd.Err(); err != nil {
+		return nil, err
+	}
+
+	values := mgetcmd.Val()
+
+	policies := make(Policies, len(values))
+	for i, v := range values {
+		p := &DefaultPolicy{}
+		b := []byte(v.(string))
+		// if !ok {
+		// 	return nil, errors.Wrapf(ErrBadConversion, "value %+v is not a byte array", v)
+		// }
+		if err := json.Unmarshal(b, p); err != nil {
+			return nil, errors.Wrap(ErrBadConversion, err.Error())
+		}
+		policies[i] = p
+	}
+
+	if offset+limit > int64(len(policies)) {
+		limit = int64(len(policies))
+		offset = 0
+	}
+
+	return policies[offset:limit], nil
 }
 
 // Get retrieves a policy.
 func (m *RedisManager) Get(id string) (Policy, error) {
-	resp, err := m.db.HGet(m.redisPoliciesKey(), id).Bytes()
-	if err == redis.Nil {
-		return nil, redisNotFound
-	} else if err != nil {
-		return nil, errors.WithStack(err)
+	var (
+		key    = prefixKey(m.keyPrefix, prefixPolicy, id)
+		cmd    = m.db.Get(key)
+		policy = &DefaultPolicy{}
+	)
+
+	if err := cmd.Err(); err != nil {
+		return nil, ErrNotFound
+	}
+	b, err := cmd.Bytes()
+	if err != nil {
+		return nil, err
 	}
 
-	return redisUnmarshalPolicy(resp)
+	if err := json.Unmarshal(b, policy); err != nil {
+		return nil, errors.Wrap(ErrBadConversion, err.Error())
+	}
+	return policy, nil
 }
 
 // Delete removes a policy.
 func (m *RedisManager) Delete(id string) error {
-	if err := m.db.HDel(m.redisPoliciesKey(), id).Err(); err != nil {
-		return errors.Wrap(err, "policy deletion failed")
+	key := prefixKey(m.keyPrefix, prefixPolicy, id)
+	getCmd := m.db.Get(key)
+	if err := getCmd.Err(); err != nil {
+		return ErrNotFound
+	}
+	policy := &DefaultPolicy{}
+	res, err := getCmd.Result()
+	if err != nil {
+		return err
+	}
+
+	if err := json.Unmarshal([]byte(res), policy); err != nil {
+		return errors.Wrap(ErrBadConversion, err.Error())
+	}
+
+	if err := m.db.Del(key).Err(); err != nil {
+		return err
+	}
+
+	// Put this policy in the hashmap for each resource
+	for _, v := range policy.GetResources() {
+		hmkey := prefixKey(m.keyPrefix, prefixResource, v)
+		field := policy.GetID()
+		if err := m.db.HDel(hmkey, field).Err(); err != nil {
+			return err
+		}
+	}
+
+	// Put this policy in the hashmap for each subject
+	for _, v := range policy.GetSubjects() {
+		hmkey := prefixKey(m.keyPrefix, prefixSubject, v)
+		field := policy.GetID()
+		if err := m.db.HDel(hmkey, field).Err(); err != nil {
+			return err
+		}
 	}
 
 	return nil
 }
 
+// FindRequestCandidates returns candidates that could match the request object. It either returns
+// a set that exactly matches the request, or a superset of it. If an error occurs, it returns nil and
+// the error.
 func (m *RedisManager) FindRequestCandidates(r *Request) (Policies, error) {
-	var ps = Policies{}
-
-	iter := m.db.HScan(m.redisPoliciesKey(), 0, "", 0).Iterator()
-	for iter.Next() {
-		if !iter.Next() {
-			break
-		}
-
-		p, err := redisUnmarshalPolicy([]byte(iter.Val()))
-		if err != nil {
-			return nil, err
-		}
-
-		ps = append(ps, p)
+	policies := Policies{}
+	var (
+		rKey    = prefixKey(m.keyPrefix, prefixResource, r.Resource)
+		sKey    = prefixKey(m.keyPrefix, prefixSubject, r.Subject)
+		rGetCmd = m.db.HGetAll(rKey)
+		sGetCmd = m.db.HGetAll(sKey)
+	)
+	if err := rGetCmd.Err(); err != nil {
+		return nil, err
+	}
+	if err := sGetCmd.Err(); err != nil {
+		return nil, err
 	}
 
-	if err := iter.Err(); err != nil {
-		return nil, errors.WithStack(err)
+	rPolicies, err := rGetCmd.Result()
+	if err != nil {
+		return nil, err
+	}
+	sPolicies, err := sGetCmd.Result()
+	if err != nil {
+		return nil, err
 	}
 
-	return ps, nil
+	for _, v := range rPolicies {
+		p := &DefaultPolicy{}
+		b := []byte(v)
+		// if !ok {
+		// 	return nil, errors.Wrapf(ErrBadConversion, "value %+v is not a byte array", v)
+		// }
+		if err := json.Unmarshal(b, p); err != nil {
+			return nil, errors.Wrap(ErrBadConversion, err.Error())
+		}
+		policies = append(policies, p)
+	}
+
+	for _, v := range sPolicies {
+		p := &DefaultPolicy{}
+		b := []byte(v)
+		// if !ok {
+		// 	return nil, errors.Wrapf(ErrBadConversion, "value %+v is not a byte array", v)
+		// }
+		if err := json.Unmarshal(b, p); err != nil {
+			return nil, errors.Wrap(ErrBadConversion, err.Error())
+		}
+		policies = append(policies, p)
+	}
+
+	return policies, nil
 }
 
-func redisUnmarshalPolicy(policy []byte) (Policy, error) {
-	var p *DefaultPolicy
-	if err := json.Unmarshal(policy, &p); err != nil {
-		return nil, errors.Wrap(err, "policy unmarshal failed")
+func (m *RedisManager) Update(policy Policy) error {
+	// Make sure that the key doesn't already exist
+	key := prefixKey(m.keyPrefix, prefixPolicy, policy.GetID())
+	if err := m.db.Get(key).Err(); err != nil {
+		return ErrNotFound
 	}
 
-	return p, nil
+	p, err := json.Marshal(policy)
+	if err != nil {
+		return err
+	}
+
+	// Set the policy key
+	cmd := m.db.Set(key, p, 0)
+
+	if err := cmd.Err(); err != nil {
+		return err
+	}
+
+	// Put this policy in the hashmap for each resource
+	for _, v := range policy.GetResources() {
+		hmkey := prefixKey(m.keyPrefix, prefixResource, v)
+		field := policy.GetID()
+		if err := m.db.HMSet(hmkey, map[string]interface{}{
+			field: p,
+		}).Err(); err != nil {
+			return err
+		}
+	}
+
+	// Put this policy in the hashmap for each subject
+	for _, v := range policy.GetSubjects() {
+		hmkey := prefixKey(m.keyPrefix, prefixSubject, v)
+		field := policy.GetID()
+		if err := m.db.HMSet(hmkey, map[string]interface{}{
+			field: p,
+		}).Err(); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
---
+++ manager/redis/manager_redis_test.go
@@ -0,0 +1,302 @@
+package redis
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/go-redis/redis"
+	"github.com/google/go-cmp/cmp"
+	. "github.com/ory/ladon"
+	"gopkg.in/ory-am/dockertest.v3"
+)
+
+var db *redis.Client
+
+func contains(s []Policy, p Policy) bool {
+	for _, v := range s {
+		if cmp.Equal(v, p) {
+			return true
+		}
+	}
+	return false
+}
+
+func TestMain(m *testing.M) {
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	// pulls an image, creates a container based on it and runs it
+	resource, err := pool.Run("redis", "3.2.11", nil)
+	if err != nil {
+		log.Fatalf("Could not start resource: %s", err)
+	}
+	settings, err := redis.ParseURL(fmt.Sprintf("redis://localhost:%s", resource.GetPort("6379/tcp")))
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	db = redis.NewClient(settings)
+
+	// exponential backoff-retry, because the application in the container might not be ready to accept connections yet
+	if err := pool.Retry(func() error {
+		_, err := db.Ping().Result()
+		if err != nil {
+			return err
+		}
+		return nil
+	}); err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	code := m.Run()
+
+	// You can't defer this because os.Exit doesn't care for defer
+	if err := pool.Purge(resource); err != nil {
+		log.Fatalf("Could not purge resource: %s", err)
+	}
+
+	os.Exit(code)
+}
+
+func TestCreate(t *testing.T) {
+	m := NewRedisManager(db, "create")
+
+	t.Run("Successfully create a single resource", func(t *testing.T) {
+		policy := &DefaultPolicy{
+			ID: "example-policy-1",
+		}
+		err := m.Create(policy)
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("Error when creating duplicate policies", func(t *testing.T) {
+		policy := &DefaultPolicy{
+			ID: "example-policy-2",
+		}
+		err := m.Create(policy)
+		if err != nil {
+			t.Fatal(err)
+		}
+		err = m.Create(policy)
+		if err != ErrPolicyExists {
+			t.Fatal("No error returned when creating duplicate policies")
+		}
+	})
+	// Create the same resource twice should return an error
+}
+
+func TestGet(t *testing.T) {
+	m := NewRedisManager(db, "get")
+
+	t.Run("Successfully retrieve a single policy", func(t *testing.T) {
+		// Some weirdness with json.Marshal requires us to initialize Conditions for this test
+		policy := &DefaultPolicy{
+			ID:         "example-policy-1",
+			Subjects:   []string{"1", "2"},
+			Conditions: Conditions{},
+		}
+		err := m.Create(policy)
+		if err != nil {
+			t.Fatal(err)
+		}
+		p, err := m.Get(policy.GetID())
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if cmp.Equal(policy, p) != true {
+			t.Fatalf("Unexpected policy.\n%s", cmp.Diff(policy, p))
+		}
+	})
+
+	t.Run("Attempt to retrieve a non-existent policy", func(t *testing.T) {
+		_, err := m.Get("policy that doesn't exist")
+		if err != ErrNotFound {
+			t.Fatal("Attempting to get a policy that doesn't exist should return an error")
+		}
+	})
+}
+
+func TestUpdate(t *testing.T) {
+	m := NewRedisManager(db, "update")
+
+	t.Run("Successfully update a policy", func(t *testing.T) {
+		// Some weirdness with json.Marshal requires us to initialize Conditions for this test
+		policy := &DefaultPolicy{
+			ID:         "example-policy-1",
+			Subjects:   []string{"1", "2"},
+			Conditions: Conditions{},
+		}
+		err := m.Create(policy)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		policy.Subjects = []string{"2", "3", "4"}
+		if err := m.Update(policy); err != nil {
+			t.Fatal(err)
+		}
+
+		u, err := m.Get(policy.GetID())
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if cmp.Equal(u, policy) != true {
+			t.Fatalf("Unexpected policy from 'Get' after 'Update'\n%s", cmp.Diff(u, policy))
+		}
+	})
+
+	t.Run("Attempt to update a policy that doesn't exist", func(t *testing.T) {
+		if err := m.Update(&DefaultPolicy{
+			ID: "this policy does not exist",
+		}); err != ErrNotFound {
+			t.Fatal("No error returned when attempting to update a policy that does not exist")
+		}
+	})
+}
+
+func TestDelete(t *testing.T) {
+	m := NewRedisManager(db, "delete")
+
+	t.Run("Successfully delete a policy", func(t *testing.T) {
+		// Some weirdness with json.Marshal requires us to initialize Conditions for this test
+		policy := &DefaultPolicy{
+			ID:         "example-policy-1",
+			Subjects:   []string{"1", "2"},
+			Conditions: Conditions{},
+		}
+		err := m.Create(policy)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if err := m.Delete(policy.GetID()); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("Attempt to delete a policy that does not exist", func(t *testing.T) {
+		if err := m.Delete("this policy does not exist"); err != ErrNotFound {
+			t.Fatal("No error returned when attempting to delete a policy that does not exist")
+		}
+	})
+}
+
+func TestFindRequestCandidate(t *testing.T) {
+	m := NewRedisManager(db, "find")
+
+	policies := Policies{
+		&DefaultPolicy{
+			ID:         "test-policy-1",
+			Subjects:   []string{"ex1", "ex2"},
+			Resources:  []string{"exr1", "exr2"},
+			Conditions: Conditions{},
+		},
+		&DefaultPolicy{
+			ID:         "test-policy-2",
+			Subjects:   []string{"ex1", "ex2"},
+			Conditions: Conditions{},
+		},
+		&DefaultPolicy{
+			ID:         "test-policy-3",
+			Subjects:   []string{"ex3", "ex4"},
+			Conditions: Conditions{},
+		},
+	}
+
+	for _, v := range policies {
+		if err := m.Create(v); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	p, err := m.FindRequestCandidates(&Request{
+		Resource: "exr1",
+		Action:   "get",
+		Subject:  "ex1",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if contains(p, policies[0]) != true {
+		t.Fatalf("Policy %+v not found in result of FindRequestCandidates", policies[0])
+	}
+
+	if contains(p, policies[2]) {
+		t.Fatalf("Policy %+v was not expected from FindRequestCandidates", policies[2])
+	}
+}
+
+func TestWithWarden(t *testing.T) {
+	m := NewRedisManager(db, "create")
+	w := &Ladon{
+		Manager: m,
+	}
+
+	policies := Policies{
+		&DefaultPolicy{
+			ID:         "test-policy-1",
+			Subjects:   []string{"user:example", "group:example"},
+			Resources:  []string{"resource:example1"},
+			Actions:    []string{"get"},
+			Effect:     AllowAccess,
+			Conditions: Conditions{},
+		},
+		&DefaultPolicy{
+			ID:         "test-policy-2",
+			Subjects:   []string{"user:example"},
+			Resources:  []string{"resource:example2"},
+			Actions:    []string{"get"},
+			Effect:     AllowAccess,
+			Conditions: Conditions{},
+		},
+		&DefaultPolicy{
+			ID:         "test-policy-3",
+			Subjects:   []string{"user:example"},
+			Resources:  []string{"resource:example3"},
+			Actions:    []string{"get"},
+			Effect:     DenyAccess,
+			Conditions: Conditions{},
+		},
+	}
+
+	for _, v := range policies {
+		if err := m.Create(v); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	type Case struct {
+		Request   *Request
+		CanAccess bool
+	}
+
+	tests := []Case{
+		{
+			Request: &Request{
+				Resource: "resource:example1",
+				Action:   "get",
+				Subject:  "user:example",
+				Context: map[string]interface{}{
+					"": nil,
+				},
+			},
+			CanAccess: true,
+		},
+	}
+
+	for i, v := range tests {
+		err := w.IsAllowed(v.Request)
+
+		if err != nil && v.CanAccess {
+			t.Errorf("Test case %d failed. Error: %s", i, err.Error())
+		}
+	}
+}
